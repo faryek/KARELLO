95f79b7968fc9068919479f7e4aa3e07ead8374c
faryek <ermak1871@gmail.com>
2023-07-16 20:43:02 +0500

    Base

---
+++ run.py
@@ -0,0 +1,32 @@
+from Organization import Ui_MainWindow
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox,
+    QStackedWidget,
+    QTableWidget,
+    QWidget
+)
+from PyQt6.QtSql import QSqlDatabase
+
+class AppWindow(QMainWindow):
+    def __init__(self):
+        super(AppWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        con = QSqlDatabase.addDatabase('QSQLITE')
+        con.setDatabaseName('WS.db')
+        try: con.open()
+        except: sys.exit(1)
+
+
+
+
+
+
+app = QApplication([])
+AppWindow = AppWindow()
+AppWindow.show()
+sys.exit(app.exec())
