6b4abcb80a5cb39d46bf32911471b52c5640eda8
Br1gery <pipecppc7@gmail.com>
2023-07-17 23:03:22 +0500

    testersxddd

--- run.py
+++ run.py
@@ -21,6 +21,23 @@ class AppWindow(QMainWindow):
         try: con.open()
         except: sys.exit(1)
 
+
+        self.ui.stackedWidget_1.setCurrentIndex(0)
+        self.ui.addChampButton.clicked.connect(lambda: self.ui.stackedWidget_1.setCurrentIndex(1)) 
+        self.ui.championshipEditButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
+        self.ui.memberButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.mainExpertButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
+        self.ui.protocolCButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+        self.ui.backToMainButton.clicked.connect(lambda: self.ui.stackedWidget_1.setCurrentIndex(0))
+        self.ui.logoutButton.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(3))
+        self.ui.logoutButton_2.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(3))
+        self.ui.memberButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(0))
+        self.ui.expertButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(1))
+        self.ui.protocolButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(2))
+        self.ui.championshipButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(2))
+        self.ui.expertButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(1))
+        self.ui.protocolButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(0))
+        self.ui.exitButton.clicked.connect(lambda: self.ui.stackedWidget_2.setCurrentIndex(0))
         
 
         self.ui.enterButton.clicked.connect(self.check_data)
@@ -67,37 +84,6 @@ class AppWindow(QMainWindow):
             self.ui.welcomeLabel.setText(f'Здравствуйте, {name}')
 
             
-
-            
-        
-
-        
-
-
-
-
-if __name__ == '__main__':
-        self.ui.stackedWidget_1.setCurrentIndex(0)
-        self.ui.enterButton.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(0)) #Kostil
-        self.ui.addChampButton.clicked.connect(lambda: self.ui.stackedWidget_1.setCurrentIndex(1)) 
-        self.ui.championshipEditButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        self.ui.memberButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
-        self.ui.mainExpertButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(3))
-        self.ui.protocolCButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
-        self.ui.backToMainButton.clicked.connect(lambda: self.ui.stackedWidget_1.setCurrentIndex(0))
-        self.ui.okButton.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(1))
-        self.ui.logoutButton.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(3))
-        self.ui.okButton.clicked.connect(lambda: self.ui.stackedWidget_2.setCurrentIndex(1)) #Kostil
-        self.ui.logoutButton_2.clicked.connect(lambda: self.ui.stackedWidget_4.setCurrentIndex(3))
-        self.ui.memberButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(0))
-        self.ui.expertButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(1))
-        self.ui.protocolButton_2.clicked.connect(lambda: self.ui.stackedWidget_3.setCurrentIndex(2))
-        self.ui.championshipButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(2))
-        self.ui.expertButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(1))
-        self.ui.protocolButton.clicked.connect(lambda: self.ui.stackedWidget3.setCurrentIndex(0))
-        self.ui.exitButton.clicked.connect(lambda: self.ui.stackedWidget_2.setCurrentIndex(0))
-
-
 if __name__ == '__main__':
     app = QApplication([])
     AppWindow = AppWindow()
