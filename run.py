78d68b960afd9bbc14a0d0ed0454f0d7f707d859
faryek <ermak1871@gmail.com>
2023-07-17 22:40:38 +0500

    Authorization and new DB data

--- run.py
+++ run.py
@@ -24,18 +24,52 @@ class AppWindow(QMainWindow):
         
 
         self.ui.enterButton.clicked.connect(self.check_data)
+        self.ui.memberEnterButton.clicked.connect(self.member_swap)
+        self.ui.okButton.clicked.connect(self.check_member)
     def check_data(self):
         login = self.ui.loginLine.text()
         password = self.ui.passLine.text()
         try: login = int(login)
-        except: sys.exit('Error!')
+        except: sys.exit(2)
+        
         query = QSqlQuery()
-        query.exec(f'SELECT COUNT(role_id) FROM Users WHERE login = {login} AND password = {password}')
-        print(f'SELECT role_id FROM Users WHERE login = {login} AND password = {password}')
-        print(type(login))
-        print(type(password))
-        print(query.first())
-        print(query.value(0))
+        try: query.exec(f'SELECT role_id, name FROM Users WHERE login = {login} AND password = {password}')
+        except: sys.exit(3)
+        if not query.next():
+            sys.exit(3)
+        role = query.record().indexOf('role_id')
+        role = query.value(role)
+        name = query.record().indexOf('name')
+        name = query.value(name)
+        query.finish()
+        
+        self.mpage_swap(role, name)
+
+    def check_member(self):
+        code = self.ui.memberCodeLine.text()
+        
+
+    def member_swap(self):
+        self.ui.stackedWidget_4.setCurrentIndex(1)
+
+    def mpage_swap(self, role, name):
+        if role in range(2, 6):
+            self.welcome(role, name)
+            self.ui.stackedWidget_4.setCurrentIndex(2)
+        elif role == 6:
+            self.welcome(role, name)
+            self.ui.stackedWidget_4.setCurrentIndex(0)
+            
+    def welcome(self, role, name):
+        if role in range(2, 6):
+            self.ui.welcomeExpertLabel.setText(f'Здравствуйте, {name}')
+        elif role == 6:
+            self.ui.welcomeLabel.setText(f'Здравствуйте, {name}')
+
+            
+
+            
+        
 
         
 
